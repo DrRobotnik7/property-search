ded4f0652c1ac91235db656ea4fb56ab8ac7e773
Hillary Mason <hillary.mason@outlook.com>
2024-02-08 20:26:35 +0000

    updated file restructure & checked merge

--- src/pages/ContactPage/ContactPage.jsx
+++ src/pages/ContactPage/ContactPage.jsx
@@ -1,10 +1,6 @@
 export default function ContactPage() {
   return (
   <>
-<<<<<<< HEAD:abode/src/components/ContactPage/ContactPage.jsx
-  <NavBar />
-=======
->>>>>>> 8b4ba46ca08cf80647262b5a1ab7fb93cbddb5cc:src/pages/ContactPage/ContactPage.jsx
   <h1>Hello Contact Page</h1>
   </>
   );
