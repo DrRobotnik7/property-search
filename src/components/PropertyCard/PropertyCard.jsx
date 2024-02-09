6f677c429456ef5a48caa919797397d6eec34d0a
Kiki <146773290+Naike-B@users.noreply.github.com>
2024-02-09 20:17:57 +0000

    Added styling for the property card

--- src/components/PropertyCard/PropertyCard.jsx
+++ src/components/PropertyCard/PropertyCard.jsx
@@ -15,27 +15,39 @@ export default function PropertyCard({
 }) {
   return (
     <>
-      <div>
-        <div>
-          <img src={imgUrl} alt={propertyName} />
-        </div>
-        <div>
-          <h2>{propertyName}</h2>
-          <p>
-            {cost}
-            <span>
-              <FontAwesomeIcon icon={faBed} /> x{bedrooms}
-            </span>
-            <span>
-              <FontAwesomeIcon icon={faShower} />
-              {bathrooms}
-            </span>
-          </p>
-          <p>Description: {description}</p>
-        </div>
-        <div>
-          <Button>Learn More</Button>
-          <span>{<FontAwesomeIcon icon={faHeart} />}</span>
+      <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-4xl xl:max-w-6xl">
+        <div className="md:flex">
+          <div className="md:shrink-0">
+            <img
+              className="h-48 w-full object-cover md:h-full md:w-60"
+              src={imgUrl}
+              alt={propertyName}
+            />
+          </div>
+          <div className="p-8 flex flex-col">
+            <p className="block mt-1 mb-3 text-xl text-left leading-tight font-bold text-primary">
+              {propertyName}
+            </p>
+            <div className="flex flex-row items-center space-x-10">
+              <p>{cost}</p>
+              <span className="space-x-4">
+                <FontAwesomeIcon icon={faBed} /> {bedrooms}
+              </span>
+              <span>
+                <FontAwesomeIcon icon={faShower} />
+                {bathrooms}
+              </span>
+            </div>
+            <p className="mt-8 text-black text-left">{description}</p>
+            <div className="flex mt-6 space-x-4 items-center">
+              <button className="mr-2 py-2 px-4 rounded-md bg-secondary text-primary">
+                Learn More
+              </button>
+              <span className="text-2xl">
+                {<FontAwesomeIcon icon={faHeart} />}
+              </span>
+            </div>
+          </div>
         </div>
       </div>
     </>
