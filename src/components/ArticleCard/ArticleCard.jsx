bb0ae5ed250ca34cbe00af90a11bf9047771d771
Hillary Mason <hillary.mason@outlook.com>
2024-02-13 21:54:56 +0000

    amended styling for Article cards, filled information for 3 cards

--- src/components/ArticleCard/ArticleCard.jsx
+++ src/components/ArticleCard/ArticleCard.jsx
@@ -3,19 +3,14 @@
 export default function ArticleCard(props) {
     return (
       <>
-        <div className="block w-96 bg-white p-10">
-          <a href="https://www.moneyhelper.org.uk/en/homes/buying-a-home/mortgage-calculator">
-            <img
-              className="w-96"
-              src="../../../src/assets/images/ArticleImg1.jpg"
-              alt="Money Helper Logo"
-            />
-          </a>
-            <p className="font-normal text-black bg-secondary">
-              Mortgages are some of the biggest commitments you’ll make in your
-              financial life. Our mortgage calculator can help.
-            </p>
-          </div>
+            <div className="flex-1 m-4">
+              <a href={props.path}>
+                <img className= "w-96 max-h-48 object-cover" src={props.image} alt={props.alt} />
+              </a>
+              <p className="font-normal text-black w-96 bg-secondary">
+                {props.paragraph}
+              </p>
+              </div>
       </>
     );
 }
