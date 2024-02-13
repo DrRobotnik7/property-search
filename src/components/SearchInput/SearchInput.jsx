0dc273ec982a0a6f4ab3971f4615709c3ed5ac9e
Kiki <146773290+Naike-B@users.noreply.github.com>
2024-02-13 19:45:33 +0000

    Added SearchInput component with controlled input state + Handled input change events and Enter key press + Clear input field after submission

--- src/components/SearchInput/SearchInput.jsx
+++ src/components/SearchInput/SearchInput.jsx
@@ -1,15 +1,30 @@
 import "./SearchInput.css";
+import React, { useState } from "react";
+
 export default function SearchInput({ onInputChange }) {
+  const [inputValue, setInputValue] = useState("");
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter") {
+      onInputChange(inputValue);
+      setInputValue("");
+    }
+  };
+
   return (
     <>
-    <h2 className="text-blue-200">TEST TEXT</h2>
       <input
         type="text"
         required
         size="10"
-        placeholder="Search postcode"
+        placeholder="Search area"
         width={"200px"}
-        onKeyUp={onInputChange}
+        value={inputValue} // Control the input with state
+        onChange={handleInputChange} // Update state on input change
+        onKeyDown={handleKeyDown} // Handle the Enter key press
       />
     </>
   );
