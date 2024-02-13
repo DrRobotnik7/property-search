a81a1b36b4206a7f4e300dc2f32dd9879d7c7347
Kiki <146773290+Naike-B@users.noreply.github.com>
2024-02-13 19:50:50 +0000

    Edited SearchInput component for user query input + Added RentFilter and SaleFilter components for property filtering options

--- src/components/JumboTron/JumboTron.jsx
+++ src/components/JumboTron/JumboTron.jsx
@@ -1,18 +1,27 @@
-import { useState } from 'react'
-import SearchInput from "../SearchInput/SearchInput"
-import background from "../../assets/images/BackgroundImage.jpg"
+import SearchInput from "../SearchInput/SearchInput";
+import background from "../../assets/images/BackgroundImage.jpg";
+import RentFilter from "../Filters/RentFilter";
+import SaleFilter from "../Filters/SaleFilter";
 
 
-const JumboTron = () => {
-    return (
-      <div className="bg-cover bg-center h-96 flex items-center justify-center relative"
-        style={{
-          backgroundImage: `url(${background})`,
-        }}
-      >
-        <SearchInput />
+const JumboTron = ({ onInputChange, isRent, setIsRent, isSale, setIsSale }) => {
+  return (
+    <div
+      className="bg-cover bg-center h-96 flex items-center justify-center relative"
+      style={{
+        backgroundImage: `url(${background})`,
+      }}
+    >
+      {/* SearchInput component for entering search terms, with a prop to handle changes */}
+      <SearchInput onInputChange={onInputChange} />
+      <div>
+        {/* RentFilter component enables and displays rent option */}
+        <RentFilter isRent={isRent} setIsRent={setIsRent} />
+        {/* SaleFilter component enables and displays sale option */}
+        <SaleFilter isSale={isSale} setIsSale={setIsSale} />
       </div>
-    );
-  };
-  
-  export default JumboTron;
+    </div>
+  );
+};
+
+export default JumboTron;
